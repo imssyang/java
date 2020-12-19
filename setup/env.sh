4ca4773dfee3051420489acae585fecf730502f9
imssyang <imssyang@gmail.com>
2020-12-19 14:48:40 +0800

    add CLASSPATH

--- setup/env.sh
+++ setup/env.sh
@@ -3,6 +3,7 @@
 export JAVA_DISABLE_ENV=yes
 export JAVA_HOME=/opt/java
 #export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF-8 -Xmx512m -Xms512m"
+#export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 export MANPATH=$JAVA_HOME/man:$MANPATH
 
