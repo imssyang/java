a08466643a313ca85b61070ec3870b8333c15e54
imssyang <imssyang@gmail.com>
2024-03-26 01:20:35 +0800

    feat: support jenv on macos

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # java
 
-/opt/java
+```bash
+ln -s /opt/java/jenv .jenv
+jenv init -
+jenv add /Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home
+jenv versions
+jenv global 17.0.10
+jenv local 17.0.10
+jenv shell oracle64-17.0.10
+```
+
--- setup/env.sh
+++ setup/env.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 export JAVA_DISABLE_ENV=yes
-export JAVA_HOME=/opt/java
+#export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 #export JAVA_OPTS="-Xms512m"
 #export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF-8 -Xmx512m -Xms512m"
 #export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
-export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
-export MANPATH=$JAVA_HOME/man:$MANPATH
-export CLASSPATH=$JAVA_HOME/sample/CJNIJava
-
+#export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
+#export MANPATH=$JAVA_HOME/man:$MANPATH
+#export CLASSPATH=$JAVA_HOME/sample/CJNIJava
 
